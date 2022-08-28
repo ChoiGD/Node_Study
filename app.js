0f9f38371974cf81892003481ccfbca446af075c
ChoiGD <ChoiGD0307@Gmail.com>
2022-08-28 23:03:33 +0900

    express res, req test

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use('/',(req, res, next)=>{
-    console.log('This always runs!');
-    next(); // 계속해서 다음 미들웨어를 실행 시키기위해서는 next()가 필수
-});
+// app.use('/',(req, res, next)=>{
+//     console.log('This always runs!');
+//     next(); // 계속해서 다음 미들웨어를 실행 시키기위해서는 next()가 필수
+// });
+
+app.use(bodyParser.urlencoded({extends: false}));
 
 app.use('/add-product',(req, res, next)=>{
     console.log('In the middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</button></form>');
+});
+
+app.use('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/',(req, res, next)=>{
