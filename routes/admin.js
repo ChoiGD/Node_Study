bfbc0013fc2ad37b335a983af71f4e5632291e57
ChoiGD <ChoiGD0307@Gmail.com>
2022-09-01 23:41:19 +0900

    model data test

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 const express = require('express');
 
-// const rootDir = require("../util/path");
-
 const productsController = require('../controllers/products');
 
 const router = express.Router();
