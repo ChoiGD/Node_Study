9f85d01af4c710dd3feba3163a88c9daaefbfacb
ChoiGD <ChoiGD0307@Gmail.com>
2022-09-13 22:43:06 +0900

    Node edit-product

--- models/cart.js
+++ models/cart.js
@@ -37,4 +37,13 @@ module.exports = class Cart {
       });
     });
   }
+
+  static deleteProduct(id, productPrice){
+    fs.readFile(p, (err, fileContent) =>{
+      if(err){
+        return;
+      }
+      const updatedCart = { ... cart };
+    });
+  }
 };
