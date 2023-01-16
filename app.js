002fc914491c0961a12aca0a65cc5aff486eef02
Choi GD <ChoiGD0307@gmail.com>
2023-01-16 11:38:52 +0900

    MongoDb + addToCart

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("63c4125d71f81e7aa319fbdc")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
