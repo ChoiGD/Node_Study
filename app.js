3139f5f772328978b84db5347ac7d63d79c84a14
Choi GD <ChoiGD0307@gmail.com>
2023-01-18 15:50:41 +0900

    Login + Session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 
@@ -19,6 +20,8 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}))
+
 
 app.use((req, res, next) => {
   User.findById('63c622cea63af8455f236876')
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,14 @@
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
-  });
+    const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
+    
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
+    });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    req.session.isLoggedIn = true;
     res.redirect('/');
-  };
+};
