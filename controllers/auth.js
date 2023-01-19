1f73f0d3e1dfe7052e2afd501490fef9ac71b5bf
Choi GD <ChoiGD0307@gmail.com>
2023-01-19 15:19:50 +0900

    Login +

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,31 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('63c79a30a0b65b9354e408f6')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+            return res.redirect('/');
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        })
+
     })
     .catch(err => console.log(err));
 };
