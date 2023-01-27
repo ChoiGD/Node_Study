235dde92113a104b395919c43fa996497aeb1ab9
Choi GD <ChoiGD0307@gmail.com>
2023-01-27 14:47:30 +0900

    multer + invoice

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
+const storage = multer.diskStorage({
+  destination: (req, file ,cb)=>{
+    cb(null, "images")
   },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
-  },
+  filename: (req, file, cb)=>{
+    cb(null, file.originalname + "-" + Date.now())
+  }
 });
 
 const fileFilter = (req, file, cb) => {
@@ -52,9 +52,10 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+  multer({ storage: storage, fileFilter: fileFilter }).single("image")
 );
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/images', express.static(path.join(__dirname, "images")));
 app.use(
   session({
     secret: "my secret",
